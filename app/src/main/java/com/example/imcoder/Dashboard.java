54e56e52b8eb3fd7ca2b50a78be9dc056e27088f
Parth <pgalani193@rku.ac.in>
2020-08-09 21:43:45 +0530

    Add activity_youtube.xml, activity_tutorial.xml, activity_game.xml, activity_contact.xml, activity_sourcecode.xml

--- app/src/main/java/com/example/imcoder/Dashboard.java
+++ app/src/main/java/com/example/imcoder/Dashboard.java
@@ -14,7 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashboard extends AppCompatActivity {
 
-    CardView mAbout,mTutorial,mYoutube,mCode,mGame,mContact;
+    CardView mAbout,mTutorial,mYoutube,mSourseode,mGame,mContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +22,11 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         mAbout      =   findViewById(R.id.About);
+        mTutorial   =   findViewById(R.id.Tutorial);
+        mYoutube    =   findViewById(R.id.Youtube);
+        mSourseode       =   findViewById(R.id.Code);
+        mGame       =   findViewById(R.id.Game);
+        mContact    =   findViewById(R.id.Contact);
 
         mAbout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +34,43 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),About.class));
             }
         });
+
+        mTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Tutorial.class));
+            }
+        });
+
+        mYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Youtube.class));
+            }
+        });
+
+        mGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Game.class));
+            }
+        });
+
+        mSourseode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Sourcecode.class));
+            }
+        });
+
+        mContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Contact.class));
+            }
+        });
+
+
     }
 
     public void Logout(View view) {
